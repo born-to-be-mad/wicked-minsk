d747de4a4b4824d090c44a0fcc0d87a5dc6238a9
Dzmitry Marudau <vinmaster@tut.by>
2020-04-28 01:11:51 +0300

    Add first component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      bases: [
+        { id: 1, name: "base 1" },
+        { id: 2, name: "base 2" },
+        { id: 3, name: "base 3" },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Wicked Minsk. Smash It!</h1>
+        {this.state.bases.map((base) => (
+          <h1 key={base.id}>{base.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
