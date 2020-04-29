611532aa0d89b34c83bb811e09f7dc28f32a9d31
Dzmitry Marudau <vinmaster@tut.by>
2020-04-30 00:56:04 +0300

    Use layour data

--- src/data/layout.data.js
+++ src/data/layout.data.js
@@ -47,6 +47,104 @@ const LAYOUT_DATA = [
                 categories: ["air", "break the circle"]
             }
         ]
+    },
+    {
+        id: 234,
+        title: "3 starts impossible",
+        imageUrl: "images/layouts/layout-123.jpg",
+        categories: ["symmetric", "rectangle", "anti-air", "single-inferno"],
+        defence: {
+            queen: 70,
+            king: 60,
+            archer: 20,
+            castleTroops: {
+                electroDragon: 1,
+                balls: 3
+            }
+        },
+        attacks: [
+            {
+                id: 101,
+                name: "Killing riders",
+                details:
+                    {
+                        videoUrl: "https://www.youtube.com/watch?v=WylJMEQ0M5k&list=PLWCehvgfrJN7h4wPWJVr9b1H7fOS9nv4W",
+                        start: "1:55",
+                        end: "4:00"
+                    },
+                troops: {
+                    hogRiders: 15,
+                    wizards: 4,
+
+                },
+                categories: ["land", "queen power"]
+            },
+            {
+                id: 202,
+                name: "Air death",
+                details:
+                    {
+                        videoUrl: "https://www.youtube.com/watch?v=WylJMEQ0M5k&list=PLWCehvgfrJN7h4wPWJVr9b1H7fOS9nv4W",
+                        start: "1:55",
+                        end: "4:00"
+                    },
+                troops: {
+                    electroDragons: 7,
+                    balls: 10,
+
+                },
+                categories: ["air", "break the circle"]
+            }
+        ]
+    },
+    {
+        id: 345,
+        title: "Hard nut to crack",
+        imageUrl: "images/layouts/layout-123.jpg",
+        categories: ["symmetric", "rectangle", "anti-air", "single-inferno"],
+        defence: {
+            queen: 70,
+            king: 60,
+            archer: 20,
+            castleTroops: {
+                electroDragon: 1,
+                balls: 3
+            }
+        },
+        attacks: [
+            {
+                id: 101,
+                name: "Killing riders",
+                details:
+                    {
+                        videoUrl: "https://www.youtube.com/watch?v=WylJMEQ0M5k&list=PLWCehvgfrJN7h4wPWJVr9b1H7fOS9nv4W",
+                        start: "1:55",
+                        end: "4:00"
+                    },
+                troops: {
+                    hogRiders: 15,
+                    wizards: 4,
+
+                },
+                categories: ["land", "queen power"]
+            },
+            {
+                id: 202,
+                name: "Air death",
+                details:
+                    {
+                        videoUrl: "https://www.youtube.com/watch?v=WylJMEQ0M5k&list=PLWCehvgfrJN7h4wPWJVr9b1H7fOS9nv4W",
+                        start: "1:55",
+                        end: "4:00"
+                    },
+                troops: {
+                    electroDragons: 7,
+                    balls: 10,
+
+                },
+                categories: ["air", "break the circle"]
+            }
+        ]
     }
 ];
 
