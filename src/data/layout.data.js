26f0394368b247c7c8dc290415d0f3b92d2def94
Dzmitry Marudau <vinmaster@tut.by>
2020-05-04 01:06:35 +0300

    Resolve conflicts for json-data

--- src/data/layout.data.js
+++ src/data/layout.data.js
@@ -1,178 +1,224 @@
 const LAYOUT_DATA = [
     {
-        id: 123,
-        title: "Magic rectangle",
-        imageUrl: "images/layouts/layout-123.jpg",
-        categories: ["symmetric", "rectangle", "anti-air", "single-inferno"],
-        defence: {
-            queen: 70,
-            king: 60,
-            archer: 20,
-            castleTroops: {
-                electroDragon: 1,
-                balls: 3
-            }
-        },
-        attacks: [
-            {
-                id: 101,
-                name: "Killing riders",
-                details:
-                    {
-                        videoUrl: "https://www.youtube.com/watch?v=WylJMEQ0M5k&list=PLWCehvgfrJN7h4wPWJVr9b1H7fOS9nv4W",
-                        start: "1:55",
-                        end: "4:00"
-                    },
-                troops: {
-                    hogRiders: 15,
-                    wizards: 4,
-
-                },
-                categories: ["land", "queen power"]
-            },
-            {
-                id: 202,
-                name: "Air death",
-                details:
-                    {
-                        videoUrl: "https://www.youtube.com/watch?v=WylJMEQ0M5k&list=PLWCehvgfrJN7h4wPWJVr9b1H7fOS9nv4W",
-                        start: "1:55",
-                        end: "4:00"
-                    },
-                troops: {
-                    electroDragons: 7,
-                    balls: 10,
-
-                },
-                categories: ["air", "break the circle"]
-            }
-        ]
-    },
-    {
-        id: 234,
-        title: "3 starts impossible",
-        imageUrl: "images/layouts/layout-123.jpg",
-        categories: ["symmetric", "rectangle", "anti-air", "single-inferno"],
-        defence: {
-            queen: 70,
-            king: 60,
-            archer: 20,
-            castleTroops: {
-                electroDragon: 1,
-                balls: 3
-            }
-        },
-        attacks: [
-            {
-                id: 101,
-                name: "Killing riders",
-                details:
-                    {
-                        videoUrl: "https://www.youtube.com/watch?v=WylJMEQ0M5k&list=PLWCehvgfrJN7h4wPWJVr9b1H7fOS9nv4W",
-                        start: "1:55",
-                        end: "4:00"
-                    },
-                troops: {
-                    hogRiders: 15,
-                    wizards: 4,
-
-                },
-                categories: ["land", "queen power"]
-            },
-            {
-                id: 202,
-                name: "Air death",
-                details:
-                    {
-                        videoUrl: "https://www.youtube.com/watch?v=WylJMEQ0M5k&list=PLWCehvgfrJN7h4wPWJVr9b1H7fOS9nv4W",
-                        start: "1:55",
-                        end: "4:00"
-                    },
-                troops: {
-                    electroDragons: 7,
-                    balls: 10,
-
-                },
-                categories: ["air", "break the circle"]
-            }
-        ]
-    },
-    {
-        id: 345,
-        title: "Hard nut to crack",
-        imageUrl: "images/layouts/layout-123.jpg",
-        categories: ["symmetric", "rectangle", "anti-air", "single-inferno"],
-        defence: {
-            queen: 70,
-            king: 60,
-            archer: 20,
-            castleTroops: {
-                electroDragon: 1,
-                balls: 3
-            }
-        },
-        attacks: [
-            {
-                id: 101,
-                name: "Killing riders",
-                details:
-                    {
-                        videoUrl: "https://www.youtube.com/watch?v=WylJMEQ0M5k&list=PLWCehvgfrJN7h4wPWJVr9b1H7fOS9nv4W",
-                        start: "1:55",
-                        end: "4:00"
-                    },
-                troops: {
-                    hogRiders: 15,
-                    wizards: 4,
-
-                },
-                categories: ["land", "queen power"]
-            },
-            {
-                id: 202,
-                name: "Air death",
-                details:
-                    {
-                        videoUrl: "https://www.youtube.com/watch?v=WylJMEQ0M5k&list=PLWCehvgfrJN7h4wPWJVr9b1H7fOS9nv4W",
-                        start: "1:55",
-                        end: "4:00"
-                    },
-                troops: {
-                    electroDragons: 7,
-                    balls: 10,
-
-                },
-                categories: ["air", "break the circle"]
-            }
-        ]
-    },
-    {
         id: 101,
-        title: "Magic rectangle #1",
+        title: "Rectangle",
         imageUrl: "images/layouts/layout-123.jpg",
-        categories: ["symmetric", "rectangle", "anti-air", "single-inferno"],
-        attacks: []
+        categories: ["multi-inferno"],
+        defence: {
+            queen: 70,
+            king: 66,
+            champion: 10,
+            varden: 50,
+            castleTroops: {
+                Ice_Golem: 1,
+                Witch: 2,
+                Archer: 6,
+            }
+        },
+        attacks: [
+            {
+                id: 1011,
+                name: "Yeti and Witch",
+                details: {
+                    videoUrl:
+                        "https://www.youtube.com/watch?v=WylJMEQ0M5k&list=PLWCehvgfrJN7h4wPWJVr9b1H7fOS9nv4W",
+                    start: "0:08",
+                    end: "1:50"
+                },
+                troops: {
+                    Yeti: 10,
+                    Witch: 8,
+                    Wizard: 1,
+                },
+                spells: {
+                    Earthquake_Spell: 8,
+                    Rage_Spell: 2,
+                    Poison_Spell: 1,
+                },
+                clanCastle: {
+                    Siege_Barracks: 1,
+                    Hog_Rider: 9,
+                },
+                categories: ["solid", "crash center"]
+            }
+        ]
+
     },
     {
         id: 102,
-        title: "Magic rectangle  #2",
-        imageUrl: "images/layouts/layout-123.jpg",
-        categories: ["symmetric", "rectangle", "anti-air", "single-inferno"],
-        attacks: []
+        title: "Rectangle",
+        imageUrl: "images/layouts/layout-002.jpg",
+        categories: ["mix-inferno"],
+        defence: {
+            queen: 70,
+            king: 70,
+            champion: 20,
+            varden: 50,
+            castleTroops: {
+                Ice_Golem: 2,
+                Witch: 1,
+                Archer: 3,
+            }
+        },
+        attacks: [
+            {
+                id: 1021,
+                name: "Yeti and Witch",
+                details: {
+                    videoUrl:
+                        "https://www.youtube.com/watch?v=WylJMEQ0M5k&list=PLWCehvgfrJN7h4wPWJVr9b1H7fOS9nv4W",
+                    start: "1:54",
+                    end: "4:00",
+                },
+                troops: {
+                    Yeti: 10,
+                    Witch: 8,
+                    Archer: 4,
+                },
+                spells: {
+                    Earthquake_Spell: 4,
+                    Rage_Spell: 3,
+                    Poison_Spell: 1,
+                    Jump_Spell: 1,
+                },
+                clanCastle: {
+                    Siege_Barracks: 1,
+                    Hog_Rider: 9
+                },
+                categories: ["solid"]
+            }
+        ]
     },
     {
         id: 103,
-        title: "Magic rectangle  #3",
-        imageUrl: "images/layouts/layout-123.jpg",
-        categories: ["symmetric", "rectangle", "anti-air", "single-inferno"],
-        attacks: []
+        title: "anti-3",
+        imageUrl: "images/layouts/layout-003.jpg",
+        categories: ["mix-inferno"],
+        defence: {
+            queen: 70,
+            king: 68,
+            champion: 11,
+            varden: 50,
+            castleTroops: {
+                Goblin: 20,
+                Archer: 25,
+            }
+        },
+        attacks: [
+            {
+                id: 1031,
+                name: "Yeti and Witch",
+                details: {
+                    videoUrl:
+                        "https://www.youtube.com/watch?v=WylJMEQ0M5k&list=PLWCehvgfrJN7h4wPWJVr9b1H7fOS9nv4W",
+                    start: "4:05",
+                    end: "5:47"
+                },
+                troops: {
+                    Yeti: 10,
+                    Witch: 8,
+                    Archer: 4
+                },
+                spells: {
+                    Earthquake_Spell: 8,
+                    Rage_Spell: 2,
+                    Poison_Spell: 1,
+                },
+                clanCastle: {
+                    Siege_Barracks: 1,
+                    Hog_Rider: 9
+                },
+                categories: ["solid", "crash center"]
+            }
+        ]
     },
     {
         id: 104,
-        title: "Magic rectangle  #4",
-        imageUrl: "images/layouts/layout-123.jpg",
-        categories: ["symmetric", "rectangle", "anti-air", "single-inferno"],
-        attacks: []
+        title: "anti-3",
+        imageUrl: "images/layouts/layout-004.jpg",
+        categories: ["single-inferno"],
+        defence: {
+            queen: 70,
+            king: 66,
+            champion: 20,
+            varden: 50,
+            castleTroops: {
+                Lava_Hound: 1,
+                Ice_Golem: 1,
+            }
+        },
+        attacks: [
+            {
+                id: 1041,
+                name: "Yeti and Witch",
+                details: {
+                    videoUrl:
+                        "https://www.youtube.com/watch?v=WylJMEQ0M5k&list=PLWCehvgfrJN7h4wPWJVr9b1H7fOS9nv4W",
+                    start: "5:51",
+                    end: "7:37"
+                },
+                troops: {
+                    Yeti: 10,
+                    Witch: 6,
+                    Wizard: 2,
+                    Ice_Golem: 1,
+                    Balloon: 1,
+                },
+                spells: {
+                    Earthquake_Spell: 8,
+                    Rage_Spell: 2,
+                    Poison_Spell: 1,
+                },
+                clanCastle: {
+                    Siege_Barracks: 1,
+                    Hog_Rider: 9
+                },
+                categories: ["solid", "crash center"]
+            }
+        ]
+    },
+    {
+        id: 105,
+        title: "anti-3",
+        imageUrl: "images/layouts/layout-005.jpg",
+        categories: ["mix-inferno", "rectangle"],
+        defence: {
+            queen: 70,
+            king: 66,
+            champion: 20,
+            varden: 50,
+            castleTroops: {
+                Ice_Golem: 3,
+            }
+        },
+        attacks: [
+            {
+                id: 1051,
+                name: "Yeti and Hog_Rider",
+                details: {
+                    videoUrl:
+                        "https://www.youtube.com/watch?v=WylJMEQ0M5k&list=PLWCehvgfrJN7h4wPWJVr9b1H7fOS9nv4W",
+                    start: "7:41",
+                    end: "9:39"
+                },
+                troops: {
+                    Yeti: 11,
+                    Hog_Rider: 5,
+                    Wizard: 3,
+                    Ice_Golem: 3,
+                },
+                spells: {
+                    Earthquake_Spell: 8,
+                    Rage_Spell: 2,
+                    Poison_Spell: 1,
+                },
+                clanCastle: {
+                    Siege_Barracks: 1,
+                    Hog_Rider: 9
+                },
+                categories: "crash center",
+            }
+        ]
     }
 ];
 
